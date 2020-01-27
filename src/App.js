6aeb84f39391ec27ea719cf3807714ab654f1cc7
VladIgolnikov <vladigolnikov@gmail.com>
2020-01-27 11:56:21 -0500

    SearchParams useState hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import Pet from "./components/Pet";
+import SearchParams from "./components/SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
+      <SearchParams />
       <Pet name="Luna" animal="dog" breed="Havanese" />
       <Pet name="Pepper" animal="bird" breed="Cockatiel" />
       <Pet name="Doink" animal="cat" breed="Mix" />
