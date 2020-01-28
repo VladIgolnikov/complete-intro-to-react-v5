92412208183c7a65bdba615b20cb9703b1c97bd3
VladIgolnikov <vladigolnikov@gmail.com>
2020-01-28 01:26:36 -0500

    error boundaries and themes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
-import { render } from "react-dom";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 import { Router, Link } from "@reach/router";
-import SearchParams from "./SearchParams";
 import Details from "./Details";
+import SearchParams from "./SearchParams";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
+  const theme = useState("darkblue");
   return (
+    <ThemeContext.Provider value={theme}>
     <div>
       <header>
         <Link to="/">Adopt Me!</Link>
@@ -15,7 +18,8 @@ const App = () => {
         <Details path="/details/:id" />
       </Router>
     </div>
+    </ThemeContext.Provider>
   );
 };
 
-render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root")); 
